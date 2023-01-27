ffe09a65e5fbd3a079f51a3f177945bdae0a1b78
devthalal <thalal.hassan@neoito.com>
2023-01-27 15:00:22 +0530

    initial commit

--- index.js
+++ index.js
@@ -8,8 +8,17 @@ const string_concat = async (req, res) => {
     res.end()
   }
 
+  const bodyBuffer = [];
+  for await (const chunk of req) {
+    bodyBuffer.push(chunk);
+  }
+  const data = Buffer.concat(bodyBuffer).toString();
+  const reqData = JSON.parse(data || "{}");
+
+  const result = reqData.str1 + reqData.str2
+
   // Add your code here
-  res.write(JSON.stringify({success: true, msg: `Hello string_concat`}))
+  res.write(JSON.stringify({success: true, msg: `string concat`, result}))
   res.end()
   
 }
