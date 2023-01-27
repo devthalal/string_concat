8262529f8fe2a173c2de5ebe78fb0739b2fb2644
devthalal <thalal.hassan@neoito.com>
2023-01-27 14:57:38 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+
+
+const string_concat = async (req, res) => {
+
+  // health check
+  if (req.params["health"] === "health") {
+    res.write(JSON.stringify({success: true, msg: "Health check success"}))
+    res.end()
+  }
+
+  // Add your code here
+  res.write(JSON.stringify({success: true, msg: `Hello string_concat`}))
+  res.end()
+  
+}
+
+export default string_concat
